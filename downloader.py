282028edccb7ad8d403faf5adc04e07acb3012b6
Alexandre Teles <ateles.1@gmail.com>
2015-01-24 14:13:36 -0300

    aria2 metalink files capabilites improved

--- downloader.py
+++ downloader.py
@@ -9,18 +9,19 @@ import os
 argparser = argparse.ArgumentParser()
 argparser.add_argument("arquivoentrada", nargs=1, help="caminho para o arquivo contendo os links para download")
 argparser.add_argument("diretoriodestino", nargs=1, help="caminho para o diretorio destino dos arquivos baixados")
-argparser.add_argument("-m, --meta", help="cria um arquivo de metalinks para download com o aria2 no diretorio de saida. nao efetua nenhum dowload.", action='store_true')
+argparser.add_argument("--criarmeta", help="cria um arquivo de metalinks para download com o aria2 no diretorio de saida. nao efetua nenhum dowload.", action='store_true')
 argparser.add_argument("--arquivometa", help="nome do arquivo de metalinks a ser salvo no diretorio destino (valido somente se usado com '--meta')")
-argparser.add_argument("-u", "--usearia2", help="caminho para o binario do aria2 (usado para downloads concorrentes)", action='store_true')
+argparser.add_argument("--usararia2", help="caminho para o binario do aria2 (usado para downloads concorrentes)", action='store_true')
 argparser.add_argument("--aria2c", nargs=1, help="caminho para o binario do aria2 (usado para downloads concorrentes. valido somente se usado com '--usearia2')")
-argparser.add_argument("-n", "--emparalelo", nargs=1, default=5, help="numero de downloads concorrents. padrao = 5 (valido soment se usado com '--aria2c'")
-argparser.add_argument("-v", "--versao", action="version", version="Software: %(prog)s | Versao: git-testing", help="imprime a versao do programa")
+argparser.add_argument("--emparalelo", nargs=1, default=5, help="numero de downloads concorrents. padrao = 5 (valido soment se usado com '--aria2c'")
+argparser.add_argument("--versao", action="version", version="Software: %(prog)s | Versao: git-testing", help="imprime a versao do programa")
 
 args = argparser.parse_args()
 
-inputfile = args.inputfile
-outputpath = args.outputpath
-ismeta = args.meta
+inputfile = args.arquivoentrada[0]
+outputpath = args.diretoriodestino[0]
+ismeta = args.criarmeta
+metafile = args.arquivometa
 
 download_array = []
 
@@ -38,6 +39,7 @@ if os.path.isfile(inputfile) and os.path.isdir(outputpath):
 			print "Download completo."
 	  		print "Tempo total: " + str(time_elapsed)
 			aux += 1
+		print "Toda a lista foi processada."
 	else:
 		filecount = -1
 		metafile = outputpath + metafile
@@ -53,8 +55,8 @@ if os.path.isfile(inputfile) and os.path.isdir(outputpath):
 				download_array = parse.html(parse.url(line))
 				metafile_object.write(download_array[0] + "\n")
 				metafile_object.write("\tout=" + download_array[1] + ".mp4\n")
-				time_elapsed = download.file(download_array[0], download_array[1], outputpath)
 				aux += 1
+		print "Toda a lista foi processada."
 elif not os.path.isfile(inputfile):
 	print "O arquivo de entrada nao existe ou tem as permissoes erradas."
 if not os.path.isdir(outputpath):
